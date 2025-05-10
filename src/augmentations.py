70e0a09c98acccdb00eca3530aa97e98b4c1f18d
Maria Shepelieva <mariia.hizhevska@stud.onu.edu.ua>
2025-05-10 10:41:43 +0300

    add augmentations

--- src/augmentations.py
+++ src/augmentations.py
@@ -3,8 +3,10 @@ from albumentations.pytorch import ToTensorV2
 
 train_transforms = A.Compose([
     A.Resize(224, 224),
-    A.MotionBlur(p=0.3),
-    A.MedianBlur(blur_limit=3, p=0.2),
+    A.OneOf([
+        A.MotionBlur(blur_limit=5),
+        A.MedianBlur(blur_limit=3)
+    ], p=0.3),
     A.OpticalDistortion(distort_limit=0.05, p=0.3),
     A.GaussNoise(p=0.3),
     A.CoarseDropout(p=0.3),
