3c470fc831cc3962774d206ee2cb18c4df7db789
hakuryuu96 <marchenkophilip@gmail.com>
2025-05-10 10:34:32 +0000

    Changed dataset, replaced data to a separate folder, fixed tests

--- src/augmentations.py
+++ src/augmentations.py
@@ -2,14 +2,13 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
 train_transforms = A.Compose([
-    A.Resize(224, 224),
     A.OneOf([
         A.MotionBlur(blur_limit=5),
         A.MedianBlur(blur_limit=3)
     ], p=0.3),
     A.OpticalDistortion(distort_limit=0.05, p=0.3),
     A.GaussNoise(p=0.3),
-    A.CoarseDropout(p=0.3),
-    A.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)),
-    ToTensorV2(),
+    A.CoarseDropout(p=0.3)
 ])
+
+valid_transforms = A.Compose([])
--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -2,16 +2,17 @@ import pytest
 import torch
 from torch.utils.data import DataLoader
 from pathlib import Path
-import sys
+import os
 
-# шлях до батьківської директорії
-sys.path.append(str(Path(__file__).resolve().parent.parent))
-from src.work import EyeDataset
+import rootutils
+rootutils.setup_root(__file__, indicator=".git", pythonpath=True)
+
+from src.dataset import EyeDataset
 
 
 @pytest.fixture(scope="module")
 def dataset():
-    dataset_path = Path(__file__).parent.parent / "src" / "EyeDataset"
+    dataset_path = Path(os.path.abspath(__file__)).parent.parent / "data" 
     return EyeDataset(root_dir=dataset_path, image_size=(224, 224))
 
 
@@ -20,13 +21,15 @@ def test_dataset_length(dataset):
 
 
 def test_image_shape(dataset):
-    image, label = dataset[0]
+    image, label, label_ohe = dataset[0]["image"], dataset[0]["label"], dataset[0]["label_ohe"]
     assert isinstance(image, torch.Tensor), "Помилка: Зображення не в форматі torch.Tensor!"
-    assert image.shape == (3, 224, 224), f"Помилка: Очікується форма (3, 224, 224), але отримали {image.shape}!"
+    assert image.shape == (1, 224, 224), f"Помилка: Очікується форма (1, 224, 224), але отримали {image.shape}!"
+    assert label_ohe.shape == (len(dataset.classes),), f"Помилка: Очікується форма ({len(dataset.classes)},), але отримали {label_ohe.shape}!"
+    assert label.dtype == torch.long, "Помилка: Мітка має бути типу torch.long!"
 
 
 def test_label_type(dataset):
-    _, label = dataset[0]
+    label = dataset[0]["label"]
     assert isinstance(label, torch.Tensor), "Помилка: Мітка не в форматі torch.Tensor!"
     assert label.dtype == torch.long, "Помилка: Мітка має бути типу torch.long!"
     assert 0 <= label < len(dataset.classes), f"Помилка: Метка {label} виходить за діапазон класів!"
@@ -35,14 +38,14 @@ def test_label_type(dataset):
 def test_dataloader(dataset):
     dataloader = DataLoader(dataset, batch_size=16, shuffle=True)
     batch = next(iter(dataloader))
-    images, labels = batch
+    images, labels = batch["image"], batch["label"]
 
-    assert images.shape == (16, 3, 224, 224), f"Помилка: неправильна форма batch {images.shape}!"
+    assert images.shape == (16, 1, 224, 224), f"Помилка: неправильна форма batch {images.shape}!"
     assert labels.shape == (16,), f"Помилка: неправильна форма міток {labels.shape}!"
 
 
 def test_multiple_images(dataset):
     indices = [0, 5, 10]
     for idx in indices:
-        image, _ = dataset[idx]
-        assert image.shape == (3, 224, 224), f"Помилка: Неправильна форма зображення для індексу {idx}!"
+        image = dataset[idx]["image"]
+        assert image.shape == (1, 224, 224), f"Помилка: Неправильна форма зображення для індексу {idx}!"
